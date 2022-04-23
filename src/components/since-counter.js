dd0ef1802b70526ed39dc397188ad036daadd4da
Conrad Hill <c.hill@parcel2go.com>
2022-04-23 21:35:45 +0100

    fixes/changes/playing with code.

--- src/components/since-counter.js
+++ src/components/since-counter.js
@@ -1,37 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function SinceCounter(props) {
+  const [since, setSince] = useState(0);
 
-    const[since, setSince] = useState(0);
-
-    // function refreshCounter() {
-    //     setSinceDate(new Date());
-    // }
-
-    useEffect(() => {
-        const timerId = setInterval(updateSince, 1000);
-        return function cleanup() {
-            clearInterval(timerId);
-        };
-    });
-
-    
-    function updateSince(){
-        let START_DATE = new Date(props.sinceDate);
-        let intlNumberFormatter = new Intl.NumberFormat("en-GB");
-        
-        const now = new Date();
-        const difference = Math.floor(
-            (now.getTime() - START_DATE.getTime()) / 1000
-        );
-        setSince( intlNumberFormatter.format(difference) );
+  useEffect(() => {
+    const timerId = setInterval(updateSince, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
     };
+  });
 
-    return (
-        <b>
-            Fun fact,  I've been a programmer for {since} seconds!
-        </b>
+  function updateSince() {
+    let START_DATE = new Date(props.sinceDate);
+    let intlNumberFormatter = new Intl.NumberFormat("en-GB");
+
+    const now = new Date();
+    const difference = Math.floor(
+      (now.getTime() - START_DATE.getTime()) / 1000
     );
+    setSince(intlNumberFormatter.format(difference));
+  }
 
+  return <b>Fun fact, I've been a programmer for {since} seconds!</b>;
 }
 export default SinceCounter;
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,30 +1,19 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { faReact, faHTML5 } from "@fortawesome/free-solid-svg-icons";
+import { faArrowAltCircleLeft } from "@fortawesome/free-regular-svg-icons";
 
 function Skills() {
+  const element = <FontAwesomeIcon icon={faArrowAltCircleLeft} />;
 
-    return(
-        <div>
-            <section id="skills">
-                <h2>Skills</h2>
-                <div id="skills-container">
-                    <i className="fab fa-html5"></i>
-                    <i className="fab fa-css3-alt"></i>
-                    <i className="fab fa-js"></i>
-                    <i className="fab fa-react"></i>
-                    <i className="fab fa-aws"></i>
-                    <i className="fab fa-sketch"></i>
-                    <i className="fab fa-figma"></i>
-                    <i className="fab fa-git"></i>
-                    <i className="fab fa-chrome"></i>
-                    <i className="fas fa-terminal"></i>
-                    <i className="fas fa-universal-access"></i>
-                    <i className="fab fa-npm"></i>
-                    <i className="fab fa-node"></i>
-                    <i className="fab fa-github"></i>
-                </div>
-            </section>
-        </div>
-    )
+  return (
+    <div>
+      <section id="skills">
+        <h2>Skills</h2>
+        <div id="skills-container">{element}</div>
+      </section>
+    </div>
+  );
 }
 
 export default Skills;
